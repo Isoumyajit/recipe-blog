040470f3edb78f778e141887e749e6eb7cd6f938
soumyajit chakraborty <soumyo.chak1999@gmail.com>
2023-07-06 01:45:05 +0530

    header added

--- src/app/Components/header/header.component.ts
+++ src/app/Components/header/header.component.ts
@@ -6,8 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  flag: boolean = false;
   navbar_list: string[] = [];
+  profile_manager: [{ item: string; icon: string }] = [{ item: '', icon: '' }];
   constructor() {
-    this.navbar_list.push('Recipe', 'Shopping List', 'Profile');
+    this.navbar_list.push('Recipe', 'Shopping List');
+    this.profile_manager.push(
+      {
+        item: 'Profile',
+        icon: 'https://img.icons8.com/ios/25/gender-neutral-user--v1.png',
+      },
+      {
+        item: 'Your Recieps',
+        icon: 'https://img.icons8.com/ios/25/cutlery.png',
+      },
+
+      {
+        item: 'Log out',
+        icon: 'https://img.icons8.com/ios/25/logout-rounded--v1.png',
+      }
+    );
+    this.flag = false;
+  }
+  showModal() {
+    this.flag = !this.flag;
   }
 }
